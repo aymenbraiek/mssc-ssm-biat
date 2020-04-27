52a1f7552bf318388b65f1bcc9698acf4c4a598d
aymenbraiek <aymenbraiek@esprit.tn>
2020-04-26 18:41:10 -0700

    Starting source -closes #1

---
+++ src/main/java/com/biat/mssc/ssm/biat/MsscSsmBiatApplication.java
@@ -0,0 +1,13 @@
+package com.biat.mssc.ssm.biat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscSsmBiatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscSsmBiatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biat/mssc/ssm/biat/MsscSsmBiatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biat.mssc.ssm.biat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmBiatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
